acc77916e37a29e5c5264890b6277354c8995681
dominikDurinic <dominic.durinic@gmail.com>
2024-03-27 19:45:04 +0100

    Task of fourth HW

---
+++ DZ-04/pokedex/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='message'>
+        <h1>Welcome to POKEDEX!</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
